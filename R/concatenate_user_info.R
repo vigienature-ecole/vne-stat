bd31bbfa848acaf7057caef2026351b1cebed550
sbenateau <sbenateau@mailodie.fr>
2023-11-29 10:56:33 +0100

    fix typo folder

--- R/concatenate_user_info.R
+++ R/concatenate_user_info.R
@@ -3,7 +3,7 @@ library("plyr")                                     # Load plyr package
 library("readr")                                    # Load readr package
 
 # read and give stats on VNE
-list_all_files <- paste0("data",list.files("data", pattern = "5a9")) 
+list_all_files <- paste0("data/",list.files("data", pattern = "5a9")) 
 data_all <- list_all_files %>% 
   lapply(read_csv) %>%                                         # Combine data sets into one data set 
   bind_rows()
@@ -15,4 +15,4 @@ if ("user_data.csv" %in% list.files("data")){
 data_all <- bind_rows(data_user, data_all)
 
 write_csv(data_all, "data/user_data.csv")
-file.remove(list_all_files)
+#file.remove(list_all_files)
