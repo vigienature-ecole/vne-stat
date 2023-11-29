4acf242db39e245b3def00d40064ddeae225df20
sbenateau <sbenateau@mailodie.fr>
2023-11-29 11:06:06 +0100

    remove library calls

--- R/concatenate_user_info.R
+++ R/concatenate_user_info.R
@@ -1,19 +1,15 @@
-library("dplyr")                                    # Load dplyr package
-library("plyr")                                     # Load plyr package
-library("readr")                                    # Load readr package
-
 # read and give stats on VNE
 list_all_files <- paste0("data/",list.files("data", pattern = "5a9")) 
-data_all <- list_all_files %>% 
-  lapply(read_csv) %>%                                         # Combine data sets into one data set 
-  bind_rows()
+data_all <- list_all_files |>
+  lapply(readr::read_csv) |>                                         # Combine data sets into one data set 
+  dplyr::bind_rows()
 
 if ("user_data.csv" %in% list.files("data")){
   data_user <- read.csv("data/user_data.csv")
-  data_all <- bind_rows(data_user, data_all)
+  data_all <- dplyr::bind_rows(data_user, data_all)
 }
 
 
 
-write_csv(data_all, "data/user_data.csv")
+readr::write_csv(data_all, "data/user_data.csv")
 #file.remove(list_all_files)
